bcdaf1c49b301826ae280952c0c03b9cd23e9c1c
Hafsa-Begum <86627326+Hafsa-Begum@users.noreply.github.com>
2025-07-05 22:25:22 +0600

    update api from patch to put method

--- src/app/controllers/books.controller.ts
+++ src/app/controllers/books.controller.ts
@@ -76,7 +76,7 @@ booksRoutes.get('/:bookId', async (req: Request, res: Response) => {
     }
 });
 
-booksRoutes.patch('/:bookId', async (req: Request, res: Response) => {
+booksRoutes.put('/:bookId', async (req: Request, res: Response) => {
     try {
         const bookId = req.params.bookId
         const updatedBody = req.body;
